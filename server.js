990d39c5a0fa409f738c4731ba0a5b1874c0d7da
ManabBala <manabbalaofficial@gmail.com>
2024-07-14 16:56:45 +0530

    updated cors at server

--- server.js
+++ server.js
@@ -3,21 +3,23 @@ import express from "express";
 import { dashboard, getTestPrivate, getTestPublicAll } from "./db.js";
 import authRouter from "./routes/auth.js";
 import testRouter from "./routes/tests.js";
+import cors from "cors";
 
 const port = 8000;
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // watch it
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept",
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // watch it
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept",
+//   );
+//   next();
+// });
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(authRouter);
 app.use(testRouter);
